93bffba1b826bfd35d08973c76270048d26be69a
Jonathan Rudenberg <jonathan@titanous.com>
2020-05-20 13:24:03 -0400

    Enable full datastore consistency for tests

--- script/test.sh
+++ script/test.sh
@@ -6,7 +6,7 @@ main() {
     export CLOUDSDK_CORE_PROJECT=test
     export DATASTORE_EMULATOR_HOST=localhost:8081
     start-stop-daemon --start --background --no-close --exec \
-      /usr/bin/gcloud -- beta emulators datastore start --no-store-on-disk
+      /usr/bin/gcloud -- beta emulators datastore start --no-store-on-disk --consistency=1
     
     while [[ "$(curl -s -o /dev/null -w ''%{http_code}'' $DATASTORE_EMULATOR_HOST)" != "200" ]]; do sleep 1; done
 

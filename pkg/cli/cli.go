11a337c1adbcb181d5743b8571b0f0d4122267d2
Jonathan Rudenberg <jonathan@titanous.com>
2020-05-29 20:26:29 -0400

    Add set-policy CLI command

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -19,8 +19,9 @@ var CLI struct {
 }
 
 type clientsCmd struct {
-	List   clientsListCmd   `kong:"cmd,help='list clients',default:'1'"`
-	Create clientsCreateCmd `kong:"cmd,help='create client'"`
+	List      clientsListCmd   `kong:"cmd,help='list clients',default:'1'"`
+	Create    clientsCreateCmd `kong:"cmd,help='create client'"`
+	SetPolicy clientsSetPolicy `kong:"cmd,name='set-policy',help='set client auth policy'"`
 }
 
 type clientsListCmd struct {
@@ -29,7 +30,7 @@ type clientsListCmd struct {
 func (c *clientsListCmd) Run(cfg *Config) error {
 	clients, err := cfg.DB.ListClients(context.Background())
 	if err != nil {
-		return nil
+		return err
 	}
 	t := table.NewWriter()
 	t.SetOutputMirror(os.Stdout)
@@ -57,4 +58,23 @@ func (cmd *clientsCreateCmd) Run(cfg *Config) error {
 	}
 	fmt.Println(id)
 	return nil
+}
+
+type clientsSetPolicy struct {
+	ClientID string   `kong:"required,name='client-id',help='client ID'"`
+	Domain   string   `kong:"required,help='G Suite domain name'"`
+	APIUser  string   `kong:"required,name='api-user',help='G Suite user email to impersonate for API calls'"`
+	Groups   []string `kong:"required,help='comma-separated group IDs'"`
+}
+
+func (t *clientsSetPolicy) Run(cfg *Config) error {
+	mut := &hubauth.ClientMutation{
+		Op: hubauth.ClientMutationOpSetPolicy,
+		Policy: hubauth.GoogleUserPolicy{
+			Domain:  t.Domain,
+			APIUser: t.APIUser,
+			Groups:  t.Groups,
+		},
+	}
+	return cfg.DB.MutateClient(context.Background(), t.ClientID, []*hubauth.ClientMutation{mut})
 }

c5955f6bda634c2adcd59f3732cc265c048bc382
Jonathan Rudenberg <jonathan@titanous.com>
2020-06-02 12:20:06 -0400

    Clean up expired resources in /cron

--- cmd/hubauth-int/main.go
+++ cmd/hubauth-int/main.go
@@ -12,10 +12,12 @@ import (
 	"contrib.go.opencensus.io/exporter/stackdriver/propagation"
 	"github.com/flynn/hubauth/pkg/clog"
 	"github.com/flynn/hubauth/pkg/datastore"
+	"github.com/flynn/hubauth/pkg/errstack"
 	"github.com/flynn/hubauth/pkg/groupsync"
 	"go.opencensus.io/plugin/ochttp"
 	"go.opencensus.io/trace"
 	"go.uber.org/zap"
+	"golang.org/x/sync/errgroup"
 )
 
 func main() {
@@ -31,7 +33,7 @@ func main() {
 		log.Fatal(err)
 	}
 	trace.RegisterExporter(exporter)
-	trace.ApplyConfig(trace.Config{DefaultSampler: trace.AlwaysSample()})
+	trace.ApplyConfig(trace.Config{DefaultSampler: trace.ProbabilitySampler(0.1)})
 
 	ctx := context.Background()
 	errClient, err := errorreporting.NewClient(ctx, os.Getenv("PROJECT_ID"), errorreporting.Config{
@@ -46,12 +48,48 @@ func main() {
 	if err != nil {
 		log.Fatalf("error initializing datastore client: %s", err)
 	}
-	ss := groupsync.New(datastore.New(dsClient), errClient)
+	ds := datastore.New(dsClient)
+	ss := groupsync.New(ds, errClient)
 
 	http.HandleFunc("/cron", func(w http.ResponseWriter, r *http.Request) {
-		if err := ss.Sync(r.Context()); err != nil {
-			clog.Logger.Error("sync error", zap.Error(err))
-		}
+		g := &errgroup.Group{}
+		g.Go(func() error {
+			if err := ss.Sync(r.Context()); err != nil {
+				clog.Logger.Error("group sync error", zap.Error(err))
+			}
+			return nil
+		})
+		g.Go(func() error {
+			ctx, span := trace.StartSpan(r.Context(), "cron.DeleteExpiredCodes")
+			defer span.End()
+			deleted, err := ds.DeleteExpiredCodes(ctx)
+			if err != nil {
+				clog.Logger.Error("delete expired codes error", zap.Error(err))
+				errClient.Report(errorreporting.Entry{
+					Error: err,
+					Stack: errstack.Format(err),
+				})
+			} else if len(deleted) > 0 {
+				clog.Logger.Info("deleted expired codes", zap.Strings("code_ids", deleted))
+			}
+			return nil
+		})
+		g.Go(func() error {
+			ctx, span := trace.StartSpan(r.Context(), "cron.DeleteExpiredRefreshTokens")
+			defer span.End()
+			deleted, err := ds.DeleteExpiredRefreshTokens(ctx)
+			if err != nil {
+				clog.Logger.Error("delete expired refresh tokens error", zap.Error(err))
+				errClient.Report(errorreporting.Entry{
+					Error: err,
+					Stack: errstack.Format(err),
+				})
+			} else if len(deleted) > 0 {
+				clog.Logger.Info("deleted expired refresh tokens", zap.Strings("refresh_token_ids", deleted))
+			}
+			return nil
+		})
+		g.Wait()
 		w.WriteHeader(http.StatusOK)
 	})
 

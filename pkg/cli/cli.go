5eb4230e4e0de9ea84cd864074d3b780c337eea0
Flavien Binet <flavien.binet@gmail.com>
2020-08-06 17:50:10 +0200

    cli: add tests

--- pkg/cli/cli.go
+++ pkg/cli/cli.go
@@ -1,13 +1,21 @@
 package cli
 
 import (
-	kms "cloud.google.com/go/kms/apiv1"
+	"context"
+
 	"github.com/flynn/hubauth/pkg/hubauth"
+	"github.com/googleapis/gax-go/v2"
+	"google.golang.org/genproto/googleapis/cloud/kms/v1"
 )
+
+type KMS interface {
+	CreateCryptoKey(context.Context, *kms.CreateCryptoKeyRequest, ...gax.CallOption) (*kms.CryptoKey, error)
+	GetPublicKey(context.Context, *kms.GetPublicKeyRequest, ...gax.CallOption) (*kms.PublicKey, error)
+}
 
 type Config struct {
 	DB  hubauth.DataStore
-	KMS *kms.KeyManagementClient
+	KMS KMS
 
 	ProjectID string
 }

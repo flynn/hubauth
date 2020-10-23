2f359bacddef82bd025b8bb26b7c07a5d2f02e3b
Flavien Binet <flavien.binet@gmail.com>
2020-10-23 17:59:37 +0200

    cloudbuild: add script to setup hubauth-int

--- script/setup-hubauth.sh
+++ script/setup-hubauth.sh
@@ -2,7 +2,6 @@
 
 set -ueo pipefail
 
-APP="hubauth-ext"
 KMS_LOCATION="global"
 KMS_KEYRING="hubauth"
 TOKEN_TYPE="Bearer" # || Biscuit
@@ -26,14 +25,32 @@ EXPECTED_HUBAUTH_EXT_ENV=(
     "RP_GOOGLE_CLIENT_SECRET"
 )
 
+EXPECTED_HUBAUTH_INT_ENV=(
+    # auto populated using gcloud 
+    "PROJECT_ID"
+)
 
-if [ $# -lt 1 ]; then 
+if [ $# -lt 2 ]; then 
     echo "Wizard to check and help configuring hubauth-ext service" 
-	echo -e "\nUsage:\n$0 [REGION] \n" 
+	echo -e "\nUsage:\n$0 [hubauth-ext | hubauth-int] [REGION] \n" 
     exit 1
 fi
 
-REGION=$1
+APP=$1
+REGION=$2
+
+case "${APP}" in
+"hubauth-ext")
+    EXPECTED_APP_ENV=${EXPECTED_HUBAUTH_EXT_ENV[@]}
+    ;;
+"hubauth-int")
+    EXPECTED_APP_ENV=${EXPECTED_HUBAUTH_INT_ENV[@]}
+    ;;
+*)
+    echo "invalid app, it must be either hubauth-ext or hubauth-int"
+    exit 1
+    ;;
+esac
 
 SPECS=$(gcloud run services describe "${APP}-${REGION}" --platform managed --region "${REGION}" --format json)
 
@@ -41,7 +58,7 @@ CONTAINER_SPECS=$(echo "${SPECS}" | jq '.spec.template.spec.containers[0]')
 IMAGE=$(echo "${CONTAINER_SPECS}" | jq -r '.image')
 
 NEW_ENVS=()
-for env in ${EXPECTED_HUBAUTH_EXT_ENV[@]}; do
+for env in ${EXPECTED_APP_ENV[@]}; do
     EXISTS=$(echo "${CONTAINER_SPECS}" | jq '.env[] | . | select(.name | contains('\"${env}\"'))')
     if [ ! -z "${EXISTS}" ]; then
         continue
@@ -63,11 +80,18 @@ for env in ${EXPECTED_HUBAUTH_EXT_ENV[@]}; do
     "BASE_URL")
         value=$(echo "${SPECS}" | jq -r '.status.url // empty')
         ;;
-    "COOKIE_KEY_SECRET" | "CODE_KEY_SECRET" | "BISCUIT_ROOT_PRIVKEY")
-        if [ "${env}" = "BISCUIT_ROOT_PRIVKEY" ] && [ "${TOKEN_TYPE}" != "Biscuit" ]; then
+    "COOKIE_KEY_SECRET" | "CODE_KEY_SECRET")
+        value=$(gcloud secrets describe "${env}" --format 'value("name")' || true)
+        if [ -z "${value}" ]; then
+            head -c 32 /dev/random | base64 -w0 | gcloud secrets create ${env} --data-file -
+            value=$(gcloud secrets describe "${env}" --format 'value("name")')
+        fi
+        value+="/versions/latest"
+        ;;
+    "BISCUIT_ROOT_PRIVKEY")
+        if [ "${TOKEN_TYPE}" != "Biscuit" ]; then
             continue
         fi
-    
         value=$(gcloud secrets describe "${env}" --format 'value("name")' || true)
         if [ -z "${value}" ]; then
             head -c 32 /dev/random | base64 -w0 | gcloud secrets create ${env} --data-file -
@@ -104,11 +128,21 @@ if [ ${#NEW_ENVS[@]} -gt 0 ]; then
     gcloud run deploy "${APP}-${REGION}" --platform managed --region "${REGION}" --image "${IMAGE}" --update-env-vars "${ENV_STR}"
 fi
 
+BASE_URL=$(gcloud run services describe "${APP}-${REGION}" --platform managed --region "${REGION}" --format json | jq -r '.status.url')
 
 # we need a first successful deployment in order to obtain the service URL. 
 # so when it was empty, and the above deploy succeeded, we can immediatly redeploy setting the BASE_URL env
-if [ -z "$(echo ${SPECS} | jq -r '.status.url // empty')" ]; then
+if [ -z "$(echo ${SPECS} | jq -r '.status.url // empty')" ] && [[ "${EXPECTED_APP_ENV[@]}" =~ "BASE_URL" ]]; then
     echo "just obtained a service url for the first time, setting BASE_URL env variable..."
-    BASE_URL=$(gcloud run services describe "${APP}-${REGION}" --platform managed --region "${REGION}" --format json | jq -r '.status.url')
     gcloud run deploy "${APP}-${REGION}" --platform managed --region "${REGION}" --image "${IMAGE}" --update-env-vars "BASE_URL=${BASE_URL}"
+fi
+
+if [ ${APP} = "hubauth-int" ] && [ ! -z ${BASE_URL} ]; then
+    EXISTS=$(gcloud scheduler jobs describe "${APP}-${REGION}-CRON" || true)
+    if [ -z ${EXISTS} ]
+        gcloud scheduler jobs create http "${APP}-${REGION}-CRON" \
+            --description "sync & cleanup task for hubauth" \
+            --schedule "0 */1 * * *" \
+            --uri "${BASE_URL}/cron" \
+            --http-method "get" \
 fi
